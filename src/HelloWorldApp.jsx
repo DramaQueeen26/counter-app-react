e18145a49ac7474475874de32d7b54da04220ad4
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-01-02 19:10:21 -0400

    Primer componente

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,13 @@
+// * Exportación independiente
+export const HelloWorldApp = () => {
+  return (
+    <div>Hello World App</div>
+  )
+}
+
+//* Exportación por defecto
+// function App() {
+//     return (<h1>Hola Mundo</h1>);
+// }
+
+// export default App;
