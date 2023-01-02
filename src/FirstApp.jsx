b1fc5c1c98a8d64fc906231078e31ae072112d61
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-01-02 19:56:18 -0400

    Agregando estilos

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,22 +1,10 @@
-
-// * No se pueden enviar promesas, es decir, asíncrona
-const getSaludo = (a, b) => {
-    return a + b;
-}
-
-const newMessage = {
-    message: 'Hola Mundo',
-    title: 'DramaQueen'
-};
-
 export const FirstApp = () => {
-    
 
     return (
         // * Para crear códigos en conjuntos sin necesidad de crear un div
         // ?  <> sinónimo de Fragment
         <>
-            <h1>{ getSaludo(2, 3) }</h1>
+            <h1>DramaQueen</h1>
             {/* <code>{ JSON.stringify( newMessage ) }</code> */}
             <p>Soy un subtitulo</p>
         </>
