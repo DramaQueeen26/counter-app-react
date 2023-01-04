51ae051808adb84be1abbe8a0cf5d3a42b796927
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-01-04 10:30:31 -0400

    Creando el componente CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HelloWorldApp } from './HelloWorldApp';
-import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
 import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root')).render( 
     <React.StrictMode>
-        <FirstApp title="Hola, soy Goku" />
+        <CounterApp value={10} />
     </React.StrictMode>
  );
