c8dc6de4ccb38ae50bdf80dd7b626f869424ed67
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-01-04 10:43:37 -0400

    Evento onClick

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,12 +1,19 @@
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
-  return (
+
+    const handleAdd = (e) => {
+        console.log({e});
+    }
+
+    return (
     <>
         <h1>CounterApp</h1>
         <h2> { value } </h2>
+
+        <button onClick={ handleAdd }> +1 </button>
     </>
-  )
+    )
 }
 
 CounterApp.propTypes = {
